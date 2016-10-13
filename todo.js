ef4fe4abca796ba9105d8a610544db0ee1c6963b
Mangku Widodo & YONI SETIAWAN <pair+mangkuwi.yonisetiawan@devbootcamp.com>
2016-10-13 13:42:24 +0700

    submit tugas todo1 sequelize

--- todo.js
+++ todo.js
@@ -1,3 +1,52 @@
-'use strict';
+"use strict"
 
-//write your code here
+var Sequelize = require('sequelize')
+var ModelTampil = require('./models')
+var td = ModelTampil.Todos
+
+
+td.help()
+td.display()
+
+process.argv.forEach((pilihan) => {
+  if(pilihan === "add"){
+    td.create({
+      task:(process.argv[3]),
+      status:"[ ]"
+    })
+  }
+})
+
+process.argv.forEach((pilihan) => {
+  if(pilihan === "delete"){
+    td.destroy({
+      where:{
+        id:(process.argv[3])
+      }
+    })
+  }
+})
+
+
+process.argv.forEach((pilihan) => {
+if(pilihan === "complete"){
+      td.update({
+        status:"[X]"
+      },
+      {
+          where:{id:(process.argv[3])}
+      })
+  }
+})
+
+
+process.argv.forEach((pilihan) => {
+if(pilihan === "uncomplete"){
+      td.update({
+        status:"[ ]",
+        where:{
+          id:(process.argv[3])
+        }
+      })
+  }
+})
